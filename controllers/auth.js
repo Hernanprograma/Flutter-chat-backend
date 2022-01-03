8afb45d1766c20e7241352eac4c533666548563a
Hernan Rodriguez Valls <hernan.r.valls@gmail.com>
2022-01-03 15:27:17 +0100

    Backend antes de crear modelo de mensajes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const { response } = require("express");
-const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
 const { generaJWT } = require("../helpers/jwt");
+
 
 
 
@@ -94,12 +96,12 @@ const login = async (req, res = response)  =>{
 }
 const renewToken = async(req, res = response) => {
 
-    const { uid } =req.body;
+    const  uid  =req.uid;
 
     try {
         const token = await generaJWT(uid);
     
-        const usuario = await Usuario.findOne( uid );
+        const usuario = await Usuario.findById( uid );
     
     
         res.json({
@@ -120,3 +122,128 @@ const renewToken = async(req, res = response) => {
 module.exports = {
     crearUsuario, login, renewToken
 }
+
+
+
+
+
+
+
+// const { response } = require('express');
+// const bcrypt = require('bcryptjs');
+
+// const Usuario = require('../models/usuario');
+// const { generaJWT } = require('../helpers/jwt');
+// const usuario = require('../models/usuario');
+
+
+// const crearUsuario = async (req, res = response ) => {
+
+//     const { email, password } = req.body;
+
+//     try {
+
+//         const existeEmail = await Usuario.findOne({ email });
+//         if( existeEmail ) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 msg: 'El correo ya está registrado'
+//             });
+//         }
+
+//         const usuario = new Usuario( req.body );
+
+//         // Encriptar contraseña
+//         const salt = bcrypt.genSaltSync();
+//         usuario.password = bcrypt.hashSync( password, salt );
+
+//         await usuario.save();
+
+//         // Generar mi JWT
+//         const token = await generaJWT( usuario.id );
+
+//         res.json({
+//             ok: true,
+//             usuario,
+//             token
+//         });
+
+
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//             ok: false,
+//             msg: 'Hable con el administrador'
+//         });
+//     }
+// }
+
+// const login = async ( req, res = response ) => {
+
+//     const { email, password } = req.body;
+
+//     try {
+        
+//         const usuarioDB = await Usuario.findOne({ email });
+//         if ( !usuarioDB ) {
+//             return res.status(404).json({
+//                 ok: false,
+//                 msg: 'Email no encontrado'
+//             });
+//         }
+
+//         // Validar el password
+//         const validPassword = bcrypt.compareSync( password, usuarioDB.password );
+//         if ( !validPassword ) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 msg: 'La contraseña no es valida'
+//             });
+//         }
+
+
+//         // Generar el JWT
+//         const token = await generaJWT( usuarioDB.id );
+        
+//         res.json({
+//             ok: true,
+//             usuario: usuarioDB,
+//             token
+//         });
+
+
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({
+//             ok: false,
+//             msg: 'Hable con el administrador'
+//         })
+//     }
+
+// }
+
+
+// const renewToken = async( req, res = response) => {
+
+//     const uid = req.uid;
+
+//     // generar un nuevo JWT, generaJWT... uid...
+//     const token = await generaJWT( uid );
+
+//     // Obtener el usuario por el UID, Usuario.findById... 
+//     const usuario = await Usuario.findById( uid );
+
+//     res.json({
+//         ok: true,
+//         usuario,
+//         token
+//     });
+
+// }
+
+
+// module.exports = {
+//     crearUsuario,
+//     login,
+//     renewToken
+// }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,35 @@
-const {io} = require('../index')
+const { comprobarJWT } = require('../helpers/jwt');
+const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require ('../controllers/socket');
 
 
 //mensajes io de socket.io
-io.on("connection", client => { 
+io.on("connection", (client) => { 
     console.log('Cliente conectado :D'); 
+    const [ valido, uid ] = comprobarJWT(client.handshake.headers['x-token']);
+    
+    // Verificar autenticacion con el token
+    if( !valido ) return client.disconnect();
+    
+    // Cliente autenticado
+    usuarioConectado( uid );
+
+    //Ingresar al usuario a una sala especifica
+    //Sala global, client.id, 61c2301015bb5e238b218c7c
+    client.join(uid);
+
+    // Escuchar del cliente el mensaje personal
+    client.on('mensaje-personal',(payload)=> {
+        console.log(payload);
+
+        io.to(payload.para).emit('mensaje-personal',payload);
+    });
+    
+
+
     client.on('disconnect', () => { 
-        console.log('Cliente desconectado :(');
+    console.log('Cliente desconectado :(');
+    usuarioDesconectado(uid);
      });
 
     // client.on('mensaje',(payload)=>{
@@ -13,4 +37,5 @@ io.on("connection", client => {
     // io.emit('mensaje',{ admin:'Nuevo Mensaje'})
 
     // });   
+
  });
